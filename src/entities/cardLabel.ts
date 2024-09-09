5706df1370cafc53d041963e83bc43ddd19a5083
Josan_S <josanalex26@gmail.com>
2024-09-09 22:30:36 +0300

    featurer(ts)cardLabel entity

---
+++ src/entities/cardLabel.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne} from 'typeorm';
+import { Card } from './cards';
+import { Label } from './labels';
+
+
+@Entity()
+export class CardLabel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  description: string;
+
+  @Column()
+  created_at: Date;
+
+  @Column()
+  updated_at: Date;
+
+  @ManyToOne(() => Label,label=>label.cardLabel)
+  label:Label
+
+  @ManyToOne(() => Card,card=>card.cardLabel)
+  card:Card
+}
+
